9f444477aac7c5dc3074159f0898d6750cd31b6c
Vic_Rain <tmddntlr@gmail.com>
2023-04-19 15:29:36 +0900

    Feat: CursorPagination 1차 로직 구현
    CursorPagination 의 1차 로직을 구현에 RestaurantScreen에 적용시켰다

--- lib/common/models/cursor_pagination_model.dart
+++ lib/common/models/cursor_pagination_model.dart
@@ -16,6 +16,8 @@ class CursorPaginationError extends CursorPaginationBase {
   });
 }
 
+
+// 데이터가 표시될때
 @JsonSerializable(
   genericArgumentFactories: true,
 )
@@ -26,6 +28,16 @@ class CursorPagination<T> extends CursorPaginationBase {
     required this.meta,
     required this.data,
   });
+
+  CursorPagination copyWith({
+    CursorPaginationMeta? meta,
+    List<T>? data,
+  }) {
+    return CursorPagination(
+      meta: meta ?? this.meta,
+      data: data ?? this.data,
+    );
+  }
 
   factory CursorPagination.fromJson(
           Map<String, dynamic> json, T Function(Object? json) fromJsonT) =>
@@ -40,6 +52,16 @@ class CursorPaginationMeta {
     required this.count,
     required this.hasMore,
   });
+
+  CursorPaginationMeta copyWith({
+    int? count,
+    bool? hasMore,
+  }) {
+    return CursorPaginationMeta(
+      count: count ?? this.count,
+      hasMore: hasMore ?? this.hasMore,
+    );
+  }
 
   factory CursorPaginationMeta.fromJson(Map<String, dynamic> json) =>
       _$CursorPaginationMetaFromJson(json);
--- lib/restaurant/views/restaurant_screen.dart
+++ lib/restaurant/views/restaurant_screen.dart
@@ -7,18 +7,56 @@ import 'package:delivery_app_tutorial/restaurant/views/restaurant_detail_screen.
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+class RestaurantScreen extends ConsumerStatefulWidget {
   const RestaurantScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      scrollListener();
+    });
+  }
+
+  void scrollListener() {
+    // 현재 위치가 최대 길이보다 조금 덜되는 위치까지 왔다면
+    // 새로운 데이터를 추가요청
+    if (_scrollController.offset >
+        _scrollController.position.maxScrollExtent - 300) {
+      // _scrollController.position.maxScrollExtent 은 스크롤이 될 수 있는 최대 위치
+      // 여기에 -300 은 바닥에 300px 만큼 떨어져 있을때 를 의미한다
+      ref.read(restaurantProvider.notifier).paginate(fetchMore: true);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final data = ref.watch(restaurantProvider);
 
+    // 완전 처음 로딩일때
     if (data is CursorPaginationLoading) {
       return const Center(
         child: CircularProgressIndicator(),
       );
     }
+
+    // 에러일때
+    if (data is CursorPaginationError) {
+      return Center(
+        child: Text(data.message),
+      );
+    }
+
+    // CursorPagination -> 데이터 표시
+    // CursorPaginationFetchingMore -> 데이터 더 가져오기
+    // CursorPaginationRefetching -> 데이터 새로고침
 
     // 캐스팅
     final cp = data as CursorPagination;
@@ -26,8 +64,20 @@ class RestaurantScreen extends ConsumerWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 15),
       child: ListView.separated(
-        itemCount: cp.data.length,
+        controller: _scrollController,
+        itemCount: cp.data.length + 1,
         itemBuilder: (context, index) {
+          if (index == cp.data.length) {
+            // index == cp.data.length 는 마지막을 의미 data 는 0부터 시작
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              child: Center(
+                child: data is CursorPaginationFetchingMore
+                    ? const CircularProgressIndicator()
+                    : const Text('마지막 데이터입니다'),
+              ),
+            );
+          }
           final pItem = cp.data[index];
 
           return GestureDetector(
